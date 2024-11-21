008dc8a565296dfcfa2cdf3d8cee2f0d4d1e0170
Dwita Yolanda Margareth Saragih <dwitayolandamargarethsaragih@Dwitas-MacBook-Air.local>
2024-11-21 09:22:17 +0700

    first commit

---
+++ src/main/java/com/java_e_wallet/e_wallet_wallet_service/EWalletWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.java_e_wallet.e_wallet_wallet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletWalletServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_e_wallet/e_wallet_wallet_service/EWalletWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_e_wallet.e_wallet_wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
